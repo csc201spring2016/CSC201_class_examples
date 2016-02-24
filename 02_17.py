b01f5b43ac4f9657986b5bad08c0245597c3ac46
csc201spring2016 <cprudence@uri.edu>
2016-02-24 05:41:47 -0500

    Added files
    new files

---
+++ 02_17.py
@@ -0,0 +1,11 @@
+countdown = 15
+
+begin = countdown
+end = 0
+step = -1
+
+for count in range(begin, end, step): 
+    print(count)
+
+
+print("Blast off!")
---
+++ forLoops.py
@@ -0,0 +1,9 @@
+list1 = [1, 2, 3, 4, 5, 6, 7]
+list2 = [8, 9, 10]
+
+#Iterating through individual elements in list2
+for i in list2:
+    #Appending these individual elements to list1
+    list1.append(i) 
+
+print (list1)
